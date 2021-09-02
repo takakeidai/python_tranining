a27e2bd174386d3f921305614a0ac56b18a052f7
島田敬大 <takahiroshimada@shimadatakashidainoMacBook-Pro.local>
2021-09-03 00:14:28 +0900

    同時に料理できる数を考慮に入れた関数の作成

--- pytra3.py
+++ pytra3.py
@@ -17,31 +17,50 @@ food_orders3 = [{'name':'french fries','wait_time':5},
                {'name':'orange juice','wait_time':4},
                 {'name':'ice cream','wait_time':2}]
 
+food_orders4 = [{'name':'french fries','wait_time':5},
+               {'name':'hamburger ','wait_time':8},
+               {'name':'wine','wait_time':2},
+               {'name':'orange juice','wait_time':4},
+                {'name':'gyoza','wait_time':14},
+                {'name':'poteto','wait_time':11},
+                {'name':'pasta','wait_time':9},
+                {'name':'pizza','wait_time':6},
+                {'name':'salad','wait_time':7},
+                {'name':'ramen','wait_time':7}]
+
 
 def calculate_wait_time(orders):
+    ## 同時に料理できる数をnumber_of_cookingに入れると、それに従った最短の料理時間が出てくる。
+    number_of_cooking = 2
     list_wait_time = []
-    for i in orders:
-        list_wait_time.append(i['wait_time'])
+    for order in orders:
+        list_wait_time.append(order['wait_time'])
         list_wait_time.sort(reverse=True)
 
-    higher_num = list_wait_time[0]
-    lower_num = list_wait_time[1]
-    for i in range(2, len(list_wait_time)):
-        sum = lower_num + list_wait_time[i]
-        lower_num = min(higher_num, sum)
-        higher_num = max(higher_num, sum)
+    higher_numbers = [list_wait_time[i] for i in range(0, number_of_cooking)]
 
-    wait_time = higher_num
-    return wait_time
+    for temporary_low_number in range(0, (len(list_wait_time) - number_of_cooking)):
+        low_num = higher_numbers[number_of_cooking-1] + list_wait_time[number_of_cooking + temporary_low_number]
+        higher_numbers.pop(number_of_cooking-1)
+        higher_numbers.append(low_num)
+        higher_numbers.sort(reverse=True)
 
-w_1 = calculate_wait_time(food_orders1)
-print(w_1)
+    waiting_time = max(higher_numbers)
 
-w_2 = calculate_wait_time(food_orders2)
-print(w_2)
+    return waiting_time
 
-w_3 = calculate_wait_time(food_orders3)
-print(w_3)
+wait_time_1 = calculate_wait_time(food_orders1)
+print(wait_time_1)
+
+wait_time_2 = calculate_wait_time(food_orders2)
+print(wait_time_2)
+
+wait_time_3 = calculate_wait_time(food_orders3)
+print(wait_time_3)
+
+wait_time_4 = calculate_wait_time(food_orders4)
+print(wait_time_4)
+
 
 
 
